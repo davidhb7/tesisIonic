0c0423e44d174be1b39538f41034404b8df44a54
davidhb7 <herrera17david@gmail.com>
2024-08-16 15:33:02 -0500

    Actualizacion API-M

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
-import { initializeApp } from "firebase/app";
 //VARIABLES DE PRODUCCION
 export const environment = {
   production: true,
@@ -11,5 +10,5 @@ export const environment = {
     messagingSenderId: "1053661105256",
     appId: "1:1053661105256:web:66d9999b5cdd683631ffdd"
   },
-  ak:"AIzaSyAFNy4BqdVggOknb2Qz9eaPEHrbv_7ddjc"
+  ak:"AIzaSyAEg4LYp3RqblvzMTvJZNPIGIAPGzbYnaw"
 };
