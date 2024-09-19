00b33eec480196fc73a22b233b1b37495f94591f
davidhb7 <herrera17david@gmail.com>
2024-09-19 11:38:53 -0500

    Clusters del mapa

--- src/app/components/heatmap/heatmap.component.ts
+++ src/app/components/heatmap/heatmap.component.ts
@@ -2,6 +2,8 @@ import { Component, AfterViewInit } from '@angular/core';
 import { ReportesI } from 'src/app/common/interfaces/reportes.interface';
 import { FireStoreService } from 'src/app/common/services/fire-store.service';
 
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
+
 declare var google: any;
 
 @Component({
@@ -13,27 +15,30 @@ export class HeatmapComponent implements AfterViewInit {
 
   map: any;
   heatmap: any;
+  markerCluster: any;
 
   localizaciones: { lat: number, lng: number, tipoAsuntoPorOperario: string, comentarioOperario: string }[] = [];
+  marcadores:any[]=[];
 
   reportes: ReportesI[] = [];
 
   constructor(private serviciosFireStoreDatabase: FireStoreService) { }
 
   ngAfterViewInit() {
-    this.initMap();
+    this.inicializarMapa();
     this.getReportesGeneralesEmpresaOperario();
   }
 
-  async initMap() {
+  //INICIALIZAR MAPA
+  async inicializarMapa() {
     const mapRef = document.getElementById('map');
-
+    //CREA EL MAPA Y LE DA UNA UBICACION INICIAL
     this.map = new google.maps.Map(mapRef, {
       center: { lat: 3.522996, lng: -76.711889 },
       zoom: 15,
       scaleControl: true,
     });
-
+    //DECLARA MAPA DE CALOR
     this.heatmap = new google.maps.visualization.HeatmapLayer({
       data: this.getPoints(),
       map: this.map,
@@ -44,11 +49,13 @@ export class HeatmapComponent implements AfterViewInit {
     this.map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
   }
 
+  //OBTIENE LAS UBICACIONES DE LOS REPORTS
   getPoints() {
     return this.localizaciones.map(
       loc => new google.maps.LatLng(loc.lat, loc.lng));
   }
 
+  //TRAE LOS REPORTES Y GUARA LAS UBICACIONES
   getReportesGeneralesEmpresaOperario() {
     this.serviciosFireStoreDatabase.getDocumentosGeneralAtentoCambios<ReportesI>("Reportes").subscribe(data => {
       if (data) {
@@ -64,18 +71,20 @@ export class HeatmapComponent implements AfterViewInit {
     });
   }
 
+  //SEPARA LAS UBICACIONES
   separadorUbicacion(coordenadas: string, tipoAsuntoPorOperario: string, comentarioOperario: string) {
     const [latStr, lngStr] = coordenadas.split(' ');
     const lat = parseFloat(latStr);
     const lng = parseFloat(lngStr);
     this.localizaciones.push({ lat, lng, tipoAsuntoPorOperario, comentarioOperario });
   }
-
+  //CARGA EL MAPA DE CALOR Y LO MANTIENE ACTUALIZADO
   updateHeatmap() {
     const points = this.getPoints();
     this.heatmap.setData(points);
   }
 
+  //AGREGAR MARCADORES SEGUN LAS COORDENADAS
   addMarkers() {
     this.localizaciones.forEach(loc => {
       const marker = new google.maps.Marker({
@@ -98,6 +107,13 @@ export class HeatmapComponent implements AfterViewInit {
       marker.addListener('click', () => {
         infoWindow.open(this.map, marker);
       });
+      this.marcadores.push(marker)
+    });
+    // Inicializar MarkerClusterer y pasar los marcadores
+    this.markerCluster = new MarkerClusterer({
+      markers: this.marcadores,
+      map: this.map, // El mapa donde quieres mostrar los clústeres
+      // Opcional: Puedes personalizar las opciones de clúster aquí
     });
   }
 }
